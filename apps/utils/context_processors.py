f6b3d26bce992a743d833dc39029311c596ebded
Ptitsyn Andrey <andrey.ptitsyn86@gmail.com>
2017-04-01 01:38:43 +0300

    refactoring templates

--- apps/utils/context_processors.py
+++ apps/utils/context_processors.py
@@ -20,7 +20,7 @@ def get_text_banner(request):
 
 def global_context(request):
     return {
-        'mobile_url': request.get_host() + 'pda/',
+        'mobile_url': request.get_host() + '/pda/',
         'rambler_id': '868956' if 'lenin' in request.path else '24858',
         'text_banner': get_text_banner(request)
     }
