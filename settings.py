a79f2a9f40e9361d06b8464c93f521778619f20c
Ptitsyn Andrey <andrey.ptitsyn86@gmail.com>
2017-04-23 14:27:56 +0300

    settings: add STATIC_ROOT

--- settings.py
+++ settings.py
@@ -116,6 +116,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
